1284bf73b0f36ace8cc3abd860a86518b46137bf
simonafranceschini <92391982+simonafranceschini@users.noreply.github.com>
2021-10-14 11:09:10 +0200

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # *Pasta alla carbonara*
+
+# ingredienti
+
+# In[25]:
+
+
+n=12
+
+
+# In[26]:
+
+
+uova= (n/4)*2
+print("numero uova", uova)
+
+
+# ![OIP.jfif](attachment:OIP.jfif)
+
+# In[ ]:
+
+
+
+
